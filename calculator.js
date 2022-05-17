eb793e757667f7d7adeb3e9e24ad070621dc1100
MaDKnight404 <battleaxe@bk.ru>
2022-05-17 16:33:50 +0300

    new idea

--- calculator.js
+++ calculator.js
@@ -6,3 +6,51 @@ const clearButton = document.querySelector('[data-clear]'),
     numberButtons = document.querySelectorAll('[data-number]'),
     operationButtons = document.querySelectorAll('[data-operation]');
 
+
+let expression = '',
+    finish = false;
+
+
+//clear
+
+function clear() {
+    expression = '';
+    operator = '';
+    currentValue.textContent = 0;
+}
+
+clearButton.addEventListener('click', clear);
+
+// Delete
+deleteButton.addEventListener('click', () => {
+    if (currentValue.textContent !== '0') {
+        currentValue.textContent = currentValue.textContent.slice(0, -1);
+    }
+})
+
+
+//Numbers
+numberButtons.forEach(element => {
+
+    element.addEventListener('click', () => {
+
+        expression += element.textContent;
+        currentValue.textContent = expression;
+    })
+
+})
+
+// Operations
+operationButtons.forEach(element => {
+
+    element.addEventListener('click', () => {
+        expression += element.textContent;
+        currentValue.textContent = expression;
+    })
+})
+
+
+equalsButton.addEventListener('click', () => {
+    previousValue.textContent = eval(expression);
+
+})
