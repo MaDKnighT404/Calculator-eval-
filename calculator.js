aeb8d7143b1ed4db3654711b63fd9dfdec7f5ba1
MaDKnight404 <battleaxe@bk.ru>
2022-05-17 12:26:26 +0300

    20%

---
+++ calculator.js
@@ -0,0 +1,8 @@
+let previousValue = document.querySelector('[data-previous__value]'),
+    currentValue = document.querySelector('[data-current__value ]');
+const clearButton = document.querySelector('[data-clear]'),
+    deleteButton = document.querySelector('[data-delete]'),
+    equalsButton = document.querySelector('[data-equals]'),
+    numberButtons = document.querySelectorAll('[data-number]'),
+    operationButtons = document.querySelectorAll('[data-operation]');
+
