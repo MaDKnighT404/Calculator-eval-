c6f4160abcd61a3dd8208d780068f55fc17a537d
MaDKnight404 <battleaxe@bk.ru>
2022-05-18 14:53:47 +0300

    complete with eval() method

--- calculator.js
+++ calculator.js
@@ -1,56 +1,66 @@
+// in this code i use eval() method, but it is strongly discouraged!
+// eval() method is very vulnerable to hackers!!!
+// NEVER use it in real project
+
 let previousValue = document.querySelector('[data-previous__value]'),
     currentValue = document.querySelector('[data-current__value ]');
 const clearButton = document.querySelector('[data-clear]'),
     deleteButton = document.querySelector('[data-delete]'),
     equalsButton = document.querySelector('[data-equals]'),
-    numberButtons = document.querySelectorAll('[data-number]'),
-    operationButtons = document.querySelectorAll('[data-operation]');
+    numberAndOperationButtons = document.querySelectorAll('[data-number-oper]');
 
-
-let expression = '',
-    finish = false;
-
-
+let expression = ''; // this variable contains an expression that will be evaluated later. String type
 //clear
-
-function clear() {
+function clear() { // function for clear all outputs and expression variable
     expression = '';
-    operator = '';
-    currentValue.textContent = 0;
+    currentValue.textContent = '0';
+    previousValue.textContent = '';
 }
+clearButton.addEventListener('click', clear); // add eventListener for button "C". It is clear all outputs and expression variable
 
-clearButton.addEventListener('click', clear);
+// Expression
+numberAndOperationButtons.forEach(element => { // iterating over a pseudo-array 
+    element.addEventListener('click', () => { // add eventListener for all buttons who contain numbers and operations (include '.')
+        if (/\D/.test(element.textContent)) { // if element contain not a digit
+            expression += element.textContent; // add this element-textcontent to variable "expression"
+            currentValue.textContent = expression // add this "expression" to output "currentValue"
+            if (isNaN(expression[expression.length - 2])) { // if second to last character is not a number (+-/.*)
+                expression = currentValue.textContent.slice(0, -1) // then delete last character of "expression"
+                expression = expression.replace(/.$/, `${element.textContent}`); // and replace last character of "expression" to pushing buttons-textContent
+                currentValue.textContent = expression; // put the result in "currentValue"
+                if (expression == '') { // for the start, if expression did't contain anything
+                    currentValue.textContent = '0'; // output 0 to "currentValue"
+                }
+            }
+        } else { // if element contain anything else ( like a number )
+            expression += element.textContent; // just add textContent of element to "expression"
+            currentValue.textContent = expression // and bring this epxression to "currentValue"
+        }
+    })
+})
 
 // Delete
 deleteButton.addEventListener('click', () => {
-    if (currentValue.textContent !== '0') {
-        currentValue.textContent = currentValue.textContent.slice(0, -1);
+    expression = currentValue.textContent.slice(0, -1);
+    currentValue.textContent = expression;
+    if (expression == '') { // if expression is empty
+        currentValue.textContent = '0'; // set currentValue
     }
 })
 
-
-//Numbers
-numberButtons.forEach(element => {
-
-    element.addEventListener('click', () => {
-
-        expression += element.textContent;
-        currentValue.textContent = expression;
-    })
-
-})
-
-// Operations
-operationButtons.forEach(element => {
-
-    element.addEventListener('click', () => {
-        expression += element.textContent;
-        currentValue.textContent = expression;
-    })
-})
-
-
+// equals
 equalsButton.addEventListener('click', () => {
-    previousValue.textContent = eval(expression);
+    if (isNaN(expression[expression.length - 1])) { // if last character in "expression" is a operation (+-/.*)
+        expression = expression.slice(0, -1); // delete last character in "expression"
+        currentValue.textContent = expression; // put the result in "currentValue"
+    } else {
+        previousValue.textContent = `= ${eval(expression)}`; // output the result of the expression to "previousValue"
+        expression = '' // clear expression
+        currentValue.textContent = '0'; // and set 0 to "currentValue"
+    }
+
+    if (previousValue.textContent == '= Infinity') {
+        previousValue.textContent = 'cannot be divided by 0'
+    }
 
 })
